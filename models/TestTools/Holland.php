5a35675892065de351588ac564689fcda112c46e
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-03 23:36:05 +0700

    fix holland post type

--- models/TestTools/Holland.php
+++ models/TestTools/Holland.php
@@ -13,7 +13,7 @@ class Holland
             $child->post_title = "Jawaban ".$post->post_title." ".$i;
             $child->post_content = $worksheet->getCellByColumnAndRow($i+1, $row)->getValue();
             $child->post_as = "Jawaban";
-            $child->post_type = $i == 2 ? 1 : 0;
+            $child->post_type = $i;
             $child->save(false);
             $child->link('parents',$post);
         }
