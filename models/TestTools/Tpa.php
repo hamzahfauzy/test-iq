c1476a3951d85e14f429a2cd1af2ee2c9e51bd5b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-04 12:21:20 +0700

    update tpa logic

--- models/TestTools/Tpa.php
+++ models/TestTools/Tpa.php
@@ -13,7 +13,7 @@ class Tpa
             $child->post_title = "Jawaban ".$post->post_title." ".$i;
             $child->post_content = $worksheet->getCellByColumnAndRow($i+1, $row)->getValue();
             $child->post_as = "Jawaban";
-            $child->post_type = $i == 2 ? 1 : 0;
+            $child->post_type = $i == 1 ? 1 : 0;
             $child->save(false);
             $child->link('parents',$post);
         }
