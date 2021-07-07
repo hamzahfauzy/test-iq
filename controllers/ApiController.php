c6d0edcdd72dff00c50cc4487af3eb88ce255b2b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-07 22:39:16 +0700

    update link

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -46,7 +46,7 @@ class ApiController extends \yii\web\Controller
     
         $this->enableCsrfValidation = false;
 
-        if($action->id == 'login' || $action->id == 'logout' || $action->id == 'data' || $action->id == 'demo-categories'){
+        if($action->id == 'login' || $action->id == 'logout' || $action->id == 'demo-categories'){
             return parent::beforeAction($action);
         }
 
