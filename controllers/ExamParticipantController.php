d6c255339e7ce132164f55fff4296794df11155e
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-13 10:57:44 +0700

    update reset

--- controllers/ExamParticipantController.php
+++ controllers/ExamParticipantController.php
@@ -317,11 +317,10 @@ class ExamParticipantController extends Controller
     {
         $model = $this->findModel($id);
 
-        ExamAnswer::deleteAll(['exam_id'=>$model->exam_id,'participant_id'=>$model->participant_id]);
+        // ExamAnswer::deleteAll(['exam_id'=>$model->exam_id,'participant_id'=>$model->participant_id]);
         ExamCategory::deleteAll(['exam_id'=>$model->exam_id,'participant_id'=>$model->participant_id]);
 
         $model->status = "";
-        $model->started_at = NULL;
         $model->finished_at = NULL;
         $model->save();
         
