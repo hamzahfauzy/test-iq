7de5b53ff7e8084f2182be572d5cc72ac1c91920
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-08-18 22:01:23 +0700

    jurusan baru

--- config/jurusan.php
+++ config/jurusan.php
@@ -8,6 +8,11 @@ return [
         'AP'=>'AP',
         'TKim'=>'TKim',
         'TM'=>'TM',
-
+        'ADK' => 'ADK',
+        'DISKOMVIS' => 'DISKOMVIS',
+        'TLAS' => 'TLAS',
+        'TELEK' => 'TELEK',
+        'BUS' => 'BUS',
+        'TSIP' => 'TSIP'
     ]
 ];
