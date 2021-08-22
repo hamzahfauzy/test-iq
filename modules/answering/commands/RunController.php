817f4f996bcbd85a379fad0ca75c1df05d3af4d4
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-08-22 22:27:19 +0700

    skip json

--- modules/answering/commands/RunController.php
+++ modules/answering/commands/RunController.php
@@ -42,7 +42,7 @@ class RunController extends Controller
                 $data = file_get_contents($file);
                 if(empty($data) || $data == null || $data == "") continue;
                 $data = json_decode($data,1);
-                if(!(json_last_error() === JSON_ERROR_NONE)) continue;
+                if($data == null) continue;
                 foreach($data as $key => $jawaban)
                 {
                     if($jawaban == null) continue;
